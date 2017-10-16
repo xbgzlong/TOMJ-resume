5285e69df88e0901b65bab292bb9c0a635e58f94
tom <935850064@qq.com>
2017-10-16 15:38:08 +0800

    改变UC浏览器没哟效果的BUG并增加项目模块

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
  * Created by Administrator on 2017/10/14.
  */
 //轮播图的实现
-var swiper = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
+var swiper = new Swiper('.tom-pics-li .swiper-container', {
+    pagination: '.tom-pics-li .swiper-pagination',
+    nextButton: '.tom-pics-li .swiper-button-next',
+    prevButton: '.tom-pics-li .swiper-button-prev',
     paginationClickable: true,
     spaceBetween: 30,
     centeredSlides: true,
@@ -13,203 +13,15 @@ var swiper = new Swiper('.swiper-container', {
     autoplayDisableOnInteraction: false
 });
 
+var swiper = new Swiper('.tom-work .swiper-container', {
+    scrollbar: '.tom-work .swiper-scrollbar',
+    scrollbarHide: true,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true
+});
 
-
-let Progress = function(init){
-    this.init(init)
-};
-Progress.prototype= {
-    init:function (init) {
-        this.el = init.el;//元素ID
-        let cvsElement = document.getElementById(this.el),//获取元素
-            ctx=cvsElement.getContext("2d"),//获取画笔
-            width = cvsElement.width,//元素宽度
-            height=cvsElement.height,//元素高度
-            degActive=0;//动态线条
-        var timers=null;//定时器
-        //停止时的角度
-        init.deg>0&&init.deg<=100?
-            this.deg = init.deg:this.deg = 100;
-        //线宽
-        init.lineWidth !== undefined?
-            this.lineWidth = init.lineWidth : this.lineWidth =20;
-        //判断宽高较小者
-        this.wh = width>height?height:width;
-
-        //设置圆的半径，默认为宽高较小者
-        init.circleRadius>0&&init.circleRadius<=this.wh/2-this.lineWidth/2?
-            this.circleRadius = init.circleRadius:this.circleRadius = this.wh/2-this.lineWidth/2;
-
-        //绘制线的背景颜色
-        init.lineBgColor !==undefined?
-            this.lineBgColor = init.lineBgColor:this.lineBgColor='#ccc';
-
-        //绘制线的颜色
-        init.lineColor !==undefined?
-            this.lineColor = init.lineColor:this.lineColor='#009ee5';
-
-        //绘制文字颜色
-        init.textColor !==undefined?
-            this.textColor = init.textColor:this.textColor='#009ee5';
-
-        //绘制文字大小
-        init.fontSize !==undefined?
-            this.fontSize = init.fontSize:this.fontSize=parseInt(this.circleRadius/2);
-
-        //执行时间
-        this.timer = init.timer;
-
-        //清除锯齿
-        if (window.devicePixelRatio) {
-            cvsElement.style.width = width + "px";
-            cvsElement.style.height = height + "px";
-            cvsElement.height = height * window.devicePixelRatio;
-            cvsElement.width = width * window.devicePixelRatio;
-            ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
-        }
-
-        //设置线宽
-        ctx.lineWidth=this.lineWidth;
-
-        clearInterval(timers);
-
-        //启动定时器
-        timers = setInterval(function(){
-            ctx.clearRect(0,0,width,height);//清除画布
-            ctx.beginPath();//开始绘制底圆
-            ctx.arc(width/2,height/2,this.circleRadius,1,8);
-            ctx.strokeStyle=this.lineBgColor;
-            ctx.stroke();
-            ctx.beginPath();//开始绘制动态圆
-            ctx.arc(width/2,height/2,this.circleRadius,-Math.PI/2,degActive*Math.PI/180-Math.PI/2);
-            ctx.strokeStyle=this.lineColor;
-            ctx.stroke();
-            //let txt=(parseInt(degActive*100/360)+"%");//获取百分比
-            var txt;
-            if(degActive*100/360>61){
-                txt="nice";
-            }else{
-                txt="fighting";
-            };
-            ctx.font=this.fontSize+"px SimHei";
-            let w=ctx.measureText(txt).width;//获取文本宽度
-            let h=this.fontSize/2;
-            ctx.fillStyle=this.textColor;
-            ctx.fillText(txt,width/2-w/2,height/2+h/2);
-            if(degActive>=this.deg/100*360){//停止定时器
-                clearInterval(timers);
-                timers=null;
-            }
-            degActive++;
-        }.bind(this),this.timer);
-
-    }
-};
-
-    var p1 = new Progress({
-        el:'skillMore1',//canvas元素id
-        deg:80,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#fff',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-    var p1 = new Progress({
-        el:'skillMore2',//canvas元素id
-        deg:80,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#fff',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-    var p1 = new Progress({
-        el:'skillMore3',//canvas元素id
-        deg:80,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#e2e2e2',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-    var p1 = new Progress({
-        el:'skillMore4',//canvas元素id
-        deg:60,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#e2e2e2',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-    var p1 = new Progress({
-        el:'skillMore5',//canvas元素id
-        deg:80,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#e2e2e2',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-    var p1 = new Progress({
-        el:'skillMore6',//canvas元素id
-        deg:80,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#e2e2e2',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-    var p1 = new Progress({
-        el:'skillMore7',//canvas元素id
-        deg:80,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#e2e2e2',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-    var p1 = new Progress({
-        el:'skillMore8',//canvas元素id
-        deg:80,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#e2e2e2',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-    var p1 = new Progress({
-        el:'skillMore9',//canvas元素id
-        deg:80,//绘制角度
-        timer:3,//绘制时间
-        lineWidth:10,//线宽
-        lineBgColor:'#e2e2e2',//底圆颜色
-        lineColor:'#356936',//动态圆颜色
-        textColor:'#000',//文本颜色
-        fontSize:25,//字体大小
-        circleRadius:50//圆半径
-    });
-
-//var tomTimer= setInterval(
-//    tomMove()
-//    ,10000);
-//clearInterval(tomTimer);
 
 //    顶部隐藏导航栏的淡出淡入
 $(function () {
@@ -252,8 +64,10 @@ $(function () {
     getsome($("#tom-findMain2"),$("#tom-main-container"),150);
     getsome($("#tom-findExp"),$("#tom-experience"),150);
     getsome($("#tom-findExp2"),$("#tom-experience"),150);
-    getsome($("#tom-findSkill"),$("#tom-skill"),150);
-    getsome($("#tom-findSkill2"),$("#tom-skill"),150);
+    getsome($("#tom-findSkill"),$("#tom-skill"),100);
+    getsome($("#tom-findSkill2"),$("#tom-skill"),100);
+    getsome($("#tom-findwork"),$("#tom-work"),50);
+    getsome($("#tom-findwork2"),$("#tom-work"),50);
 
     //这是一个demo，
     //$(".tom-skillPic canvas").addClass("hidecss");
@@ -267,7 +81,7 @@ $(function () {
     //$("#tom-findSkill").click(function () {
     //    setTimeout(tomMove(),1000);
     //});
-    //
+    //window 滚动事件，
     //$(window).scroll(function(){
     //    var tomBig = $(window).scrollTop() - $("#tom-skill").offset().top + 150;
     //    if(tomBig > 0){
